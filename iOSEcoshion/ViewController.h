b805cc45ded15a06fc7ced9ab7d922a603666c93
马俊 <wajmjjj@gmail.com>
2014-12-22 11:21:15 +0800

    Initial Commit

---
+++ iOSEcoshion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSEcoshion
+//
+//  Created by 马俊 on 12/22/14.
+//  Copyright (c) 2014 ChaseFuture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSEcoshion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSEcoshion
+//
+//  Created by 马俊 on 12/22/14.
+//  Copyright (c) 2014 ChaseFuture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
