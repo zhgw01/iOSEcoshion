15756dac28a26991e6d6b6f6219e8fe082b76c67
马俊 <wajmjjj@gmail.com>
2014-12-24 16:40:11 +0800

    add Appearance Manager;

---
+++ iOSEcoshion/Utils/AppearanceManager.h
@@ -0,0 +1,13 @@
+//
+//  AppearanceManager.h
+//  iOSEcoshion
+//
+//  Created by 马俊 on 12/24/14.
+//  Copyright (c) 2014 ChaseFuture. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppearanceManager : NSObject
+
+@end
---
+++ iOSEcoshion/Utils/AppearanceManager.m
@@ -0,0 +1,13 @@
+//
+//  AppearanceManager.m
+//  iOSEcoshion
+//
+//  Created by 马俊 on 12/24/14.
+//  Copyright (c) 2014 ChaseFuture. All rights reserved.
+//
+
+#import "AppearanceManager.h"
+
+@implementation AppearanceManager
+
+@end
