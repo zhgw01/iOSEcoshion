58ea349b050563b9b68588d3aff97855db86b80c
马俊 <wajmjjj@gmail.com>
2014-12-25 11:38:25 +0800

    add RootViewController; adjust register steps;

---
+++ iOSEcoshion/Controller/Login/RootViewController.m
@@ -0,0 +1,38 @@
+//
+//  RootViewController.m
+//  iOSEcoshion
+//
+//  Created by 马俊 on 12/25/14.
+//  Copyright (c) 2014 ChaseFuture. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
