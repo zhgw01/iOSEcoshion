b4fbbd672ae07799ba09579fc942d9635bbee223
马俊 <wajmjjj@gmail.com>
2014-12-25 18:26:52 +0800

    add Binding Mail View Controller;

---
+++ iOSEcoshion/Controller/Login/BindingMailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BindingMailViewController.h
+//  iOSEcoshion
+//
+//  Created by 马俊 on 12/25/14.
+//  Copyright (c) 2014 ChaseFuture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BindingMailViewController : UIViewController
+
+@end
---
+++ iOSEcoshion/Controller/Login/BindingMailViewController.m
@@ -0,0 +1,39 @@
+//
+//  BindingMailViewController.m
+//  iOSEcoshion
+//
+//  Created by 马俊 on 12/25/14.
+//  Copyright (c) 2014 ChaseFuture. All rights reserved.
+//
+
+#import "BindingMailViewController.h"
+
+@interface BindingMailViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
+
+@end
+
+@implementation BindingMailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
