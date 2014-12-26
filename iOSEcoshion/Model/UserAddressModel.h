b272ae96d8d19274f2cd9c3e1f23dacfc1f5fce4
马俊 <wajmjjj@gmail.com>
2014-12-26 16:21:13 +0800

    add UserAddressModel;

---
+++ iOSEcoshion/Model/UserAddressModel.h
@@ -0,0 +1,13 @@
+//
+//  UserAddressModel.h
+//  iOSEcoshion
+//
+//  Created by 马俊 on 12/26/14.
+//  Copyright (c) 2014 ChaseFuture. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserAddressModel : NSObject
+
+@end
---
+++ iOSEcoshion/Model/UserAddressModel.m
@@ -0,0 +1,13 @@
+//
+//  UserAddressModel.m
+//  iOSEcoshion
+//
+//  Created by 马俊 on 12/26/14.
+//  Copyright (c) 2014 ChaseFuture. All rights reserved.
+//
+
+#import "UserAddressModel.h"
+
+@implementation UserAddressModel
+
+@end
